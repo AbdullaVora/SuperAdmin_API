8232d40aaf74b3c23cbd7237f5130b64a60c71bd
Sridix Tech <team.sridix@gmail.com>
2025-05-09 11:13:03 +0530

    auth token setup

--- routers/Dashboard/banner_config/couponRoute.js
+++ routers/Dashboard/banner_config/couponRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { createCoupon, getAllCoupons, getCouponById, updateCoupon, deleteCoupon } = require("../../../controllers/Dashboard/banner_config/couponController");
+const authMiddleware = require("../../../middleware/authToken");
 
 // Routes for coupon CRUD operations
-router.post("/addCoupon", createCoupon);
+router.post("/addCoupon", authMiddleware, createCoupon);
 router.get("/coupons", getAllCoupons);
 router.get("/couponsById/:id", getCouponById);
-router.put("/updateCoupon/:id", updateCoupon);
-router.delete("/deleteCoupon/:id", deleteCoupon);
+router.put("/updateCoupon/:id", authMiddleware, updateCoupon);
+router.delete("/deleteCoupon/:id", authMiddleware, deleteCoupon);
 
 module.exports = router;
